10d58e5bfdfe1cbc3733d4f2a96a3ea57d09657a
DanukaH <danunilminda@yahoo.com>
2022-12-06 01:57:24 +0000

    Look and feel for Devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+
+  validates :email, presence: true,
+                    format: { with: EMAIL_REGEX, message: 'Invalid Email' },
+                    uniqueness: { case_sensitive: false },
+                    length: { minimum: 4, maximum: 254 },
+                    confirmation: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    confirmations: 'users/confirmations'
+    passwords: 'users/passwords'
   }
+
+  devise_scope :user do
+    get '/login' => 'users/sessions#new'
+    get '/logout' => 'devise/sessions#destroy'
+    get '/users/confirm' => 'registrations#confirm'
+    post '/users/invitations' => 'users/invitations#resend'
+  end
+
   root to: 'home#index'
 end
