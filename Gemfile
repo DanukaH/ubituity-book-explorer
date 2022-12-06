bf4a84db224aeaabad75e2a6c943b6391c9f3555
DanukaH <danunilminda@yahoo.com>
2022-12-06 20:14:22 +0000

    Upload to S3 with Heroku - attempt  - 2 - fog-aws

--- Gemfile
+++ Gemfile
@@ -51,10 +51,9 @@ gem 'bootsnap', require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
-gem 'aws-sdk', '~> 2'
+gem 'aws-sdk'
 gem 'aws-sdk-rails'
 gem 'carrierwave'
-gem 'carrierwave-aws'
 gem 'devise'
 gem 'figaro'
 gem 'fog-aws'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,15 @@
 
 CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
-  config.aws_acl    = 'public-read'
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
-  config.aws_credentials = {
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    region: ENV.fetch('AWS_REGION')
+
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION']
   }
+  config.fog_directory = ENV['S3_BUCKET_NAME']
+  config.fog_public    = false
+  config.cache_dir     = "#{Rails.root}/tmp/uploads"
+  config.storage       = :fog
+
 end
